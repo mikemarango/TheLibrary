be398f1980b71f7975e226ed95967ab7dad0614b
Michael Marango <mike.marango@gmail.com>
2018-01-08 03:27:46 -0800

    added repository

---
+++ Library.API/Library.API/Services/LibService/ILibraryRepository.cs
@@ -0,0 +1,25 @@
+﻿using Library.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.API.Services.LibService
+{
+    public interface ILibraryRepository
+    {
+        IEnumerable<Author> GetAuthors();
+        Author GetAuthor(Guid authorId);
+        IEnumerable<Author> GetAuthors(IEnumerable<Guid> authorIds);
+        void CreateAuthor(Author author);
+        void DeleteAuthor(Author author);
+        void UpdateAuthor(Author author);
+        bool AuthorExists(Guid authorId);
+        IEnumerable<Book> GetBooks(Guid authorId);
+        Book GetBook(Guid authorId, Guid bookId);
+        void CreateBook(Guid authorId, Book book);
+        void UpdateBook(Book book);
+        void DeleteBook(Book book);
+        bool Save();
+    }
+}
---
+++ Library.API/Library.API/Services/LibService/LibraryRepository.cs
@@ -0,0 +1,107 @@
+﻿using Library.API.Data;
+using Library.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.API.Services.LibService
+{
+    public class LibraryRepository : ILibraryRepository
+    {
+        public LibraryContext Context { get; }
+
+        public LibraryRepository(LibraryContext context)
+        {
+            Context = context;
+        }
+
+        public IEnumerable<Author> GetAuthors()
+        {
+            return Context.Authors.OrderBy(a => a.FirstName).ThenBy(a => a.LastName);
+        }
+
+        public IEnumerable<Author> GetAuthors(IEnumerable<Guid> authorIds)
+        {
+            return Context.Authors.Where(a => authorIds.Contains(a.Id))
+                .OrderBy(a => a.FirstName).OrderBy(a => a.LastName).ToList();
+        }
+
+        public Author GetAuthor(Guid authorId)
+        {
+            return Context.Authors.FirstOrDefault(a => a.Id == authorId);
+        }
+
+        public void CreateAuthor(Author author)
+        {
+            author.Id = Guid.NewGuid();
+            Context.Authors.Add(author);
+
+            // the repository fills the id (instead of using identity columns)
+            if (author.Books.Any())
+            {
+                foreach (var book in author.Books)
+                {
+                    book.Id = Guid.NewGuid();
+                }
+            }
+        }
+
+        public void UpdateAuthor(Author author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AuthorExists(Guid authorId)
+        {
+            return Context.Authors.Any(a => a.Id == authorId);
+        }
+
+        public void DeleteAuthor(Author author)
+        {
+            Context.Authors.Remove(author);
+        }
+
+        public IEnumerable<Book> GetBooks(Guid authorId)
+        {
+            return Context.Books
+                .Where(b => b.AuthorId == authorId).OrderBy(b => b.Title).ToList();
+        }
+        
+        public Book GetBook(Guid authorId, Guid bookId)
+        {
+            return Context.Books
+                .Where(b => b.AuthorId == authorId && b.Id == bookId).FirstOrDefault();
+        }
+
+        public void CreateBook(Guid authorId, Book book)
+        {
+            var author = GetAuthor(authorId);
+            if (author != null)
+            {
+                // if there isn't an id filled out (ie: we're not upserting),
+                // we should generate one
+                if (book.Id == Guid.Empty)
+                    book.Id = Guid.NewGuid();
+
+                author.Books.Add(book);
+            }
+        }
+
+        public void UpdateBook(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteBook(Book book)
+        {
+            Context.Books.Remove(book);
+        }
+
+        public bool Save()
+        {
+            return (Context.SaveChanges() >= 0);
+        }
+
+    }
+}

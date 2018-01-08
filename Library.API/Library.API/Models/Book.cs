2b22c30591a44267eb0c421c9ffbd83ad1034837
Michael Marango <mike.marango@gmail.com>
2018-01-08 00:45:09 -0800

    added entities

---
+++ Library.API/Library.API/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.API.Models
+{
+    public class Book
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required, MaxLength(100)]
+        public string Title { get; set; }
+        [Required, MaxLength(500)]
+        public string Description { get; set; }
+        [ForeignKey("AuthorId")]
+        public Author Author { get; set; }
+        public Guid AuthorID { get; set; }
+    }
+}

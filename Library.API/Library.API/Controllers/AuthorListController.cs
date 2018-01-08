6b96a4452e6cd2d3f9ce761fd40596eb6f367f8a
Michael Marango <mike.marango@gmail.com>
2018-01-08 10:13:46 -0800

    added author collection controller

---
+++ Library.API/Library.API/Controllers/AuthorListController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Library.API.DTOs;
+using Library.API.Models;
+using Library.API.Services.LibService;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Library.API.Controllers
+{
+    [Route("api/author_list")]
+    public class AuthorListController : Controller
+    {
+        public ILibraryRepository Repository { get; }
+
+        public AuthorListController(ILibraryRepository repository)
+        {
+            Repository = repository;
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public IActionResult Post([FromBody]IEnumerable<AuthorCreateDto> authorList)
+        {
+            if (authorList == null)
+                return BadRequest();
+
+            var authors = Mapper.Map<IEnumerable<Author>>(authorList);
+
+            foreach (var author in authors)
+            {
+                Repository.CreateAuthor(author);
+            }
+
+            if (!Repository.Save())
+                throw new Exception("Unsuccessful creation of authors collection.");
+
+            return Ok();
+        }
+
+    }
+}
